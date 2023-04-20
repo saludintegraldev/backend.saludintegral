2ca6b4e99f994047fb882c992738047970cce66f
Enrique Angrisano <milangrisano@gmail.com>
2023-04-19 23:30:15 -0500

    local function

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,12 @@ import { InsurancesModule } from './insurances/insurances.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      // ssl: process.env.STAGE === 'prod',
+      // extra: {
+      //   ssl: process.env.STAGE === 'prod'
+      //         ? { rejectUnauthorized: false }
+      //         : null,
+      // },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
