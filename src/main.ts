8dddb45e6651db1b074dd4168adff6d09c69dcae
Enrique Angrisano <milangrisano@gmail.com>
2023-04-23 01:39:48 -0500

    nuevo

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT);
-  logger.log(`Salud Integral App running on port ${ process.env.PORT }`)
+  logger.log(`Salud Integral running on port ${ process.env.PORT }`)
 }
 bootstrap();
